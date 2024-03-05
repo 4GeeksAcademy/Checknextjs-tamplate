08c37034f865f6f82f85a363a17827b36fa81ccd
Matz72 <156270489+Matz72@users.noreply.github.com>
2024-03-05 01:11:28 +0000

    template

---
+++ src/pages/about.jsx
@@ -0,0 +1,10 @@
+import link from "next/link";
+import { Container } from "react-bootstrap";
+
+export default function about() {
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
---
+++ src/pages/index.jsx
@@ -0,0 +1,8 @@
+
+import { SimpleCounter } from "@/components/SimpleCounter";
+
+export default function Home() {
+  return (
+<SimpleCounter seconds={67}/>
+  )
+}
